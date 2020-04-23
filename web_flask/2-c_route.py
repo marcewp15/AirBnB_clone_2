ae353f2e746c93d672dc13fdd851211ba92e9c6c
marcewp15 <970@holbertonschool.com>
2020-04-23 05:32:04 +0000

    [Add] Python script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_isfun(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
